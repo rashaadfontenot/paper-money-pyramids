39856f1c88b8be3a68f63490c5289bb46490697a
Rashaad <rashaad@rashaadfontenot.com>
2017-04-14 06:28:21 -0700

    Created my first scraper!!!!

--- app/controllers/scraper.rb
+++ app/controllers/scraper.rb
@@ -1,9 +1,13 @@
+
 require 'nokogiri'
 require 'open-uri'
 
 url = 'http://www.nba.com/players'
 html = open(url)
 
-@doc = Nokogiri::HTML(open('http://www.nba.com/players'))
-@doc.xpath("a title")
-puts @doc
+doc = Nokogiri::HTML(html)
+
+names = doc.css('a').first.attr('title')
+
+
+puts names
